17c71da5b21e6be4d4ae0c61aefd445ac100c8a1
roggimario99-commits <roggimario99@gmail.com>
2025-11-21 16:55:25 +0100

    modifica data nascita

--- professore.py
+++ professore.py
@@ -1,7 +1,10 @@
-from persona import Persona
+from persona import Person as Persona
 
 class Professore(Persona): # Classe figlio di "Person" (per i Professori)
-    def __init__(self, nome: str, eta: int, materia: str):
-        super().__init__(nome)
+    def __init__(self, nome: str, data_nascita: str, materia: str):
+        super().__init__(nome, data_nascita)
         
         self.materia = materia # Professore
+        
+    def presentazione(self) -> str:
+        return f"nome: {self._nome}, età: {self._eta}, materia: {self.materia}"
