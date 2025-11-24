8b237166b251f428e80dd37339ca293e62d00b56
Emanuele <emanueleanzellotti@gmail.com>
2025-11-24 09:42:19 +0100

    upload

--- persona.py
+++ persona.py
@@ -11,5 +11,19 @@ class Persona: # Classe base
         self._nome = nome
         self._data_nascita = data_nascita
     
+    @property
+    def nome(self):
+        return self._nome
+    
+    @property
+    def data_nascita(self):
+        return self._data_nascita
+    
+    def set_nome(self, nuovo_nome):
+        self._nuovo_nome = nuovo_nome
+    
+    def set_nome(self, nuova_data_nascita):
+        self._data_nascita = nuova_data_nascita
+    
     def info(self):
         return f"Nome: {self.nome} (data di nascita: {self.data_nascita})"
